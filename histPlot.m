5411b2f4de0aec956d2ef5a7d16828c409fdcc04
PratipRana <ranap@vcu.edu>
2019-03-28 02:00:17 -0400

    periodic boundary done

--- histPlot.m
+++ histPlot.m
@@ -1,14 +1,8 @@
-x= rmmissing(parHitTable);
 
-count=1
-for j=1:400
-    if(sum(x(:,1)==j)>2)
-        y=sortrows(x(x(:,1)==j,:),3);
-        length(y)
-        for i=1:length(y)-1
-            p(count)= y(i+1,3)-y(i,3);
-            count=count+1;
-        end
-    end
-end
+CollitionTime (1:1000,:)=[];
+size(CollitionTime)
+histogram(CollitionTime((CollitionTime(:,2) == 2),1))
 
+fid =fopen("data.csv", 'w')
+fprintf(fid, "%f\n",CollitionTime((CollitionTime(:,2) == 2),1))
+fclose(fid)
